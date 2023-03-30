4aa241e245e6d541b0fbabfa9e1ca5b79f75a186
GuVictory <gubanova.vi10@gmail.com>
2023-03-31 02:00:41 +0300

    fix: routing

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,44 +1,47 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { Root } from '../pages/Root';
 import { Error } from '../pages/Error';
 import { Main } from '../pages/Main';
 
-export const router = createHashRouter([
-    {
-        path: '/*',
-        element: <Root />,
-        errorElement: <Error />,
-        children: [
-            {
-                path: '',
-                element: <Main />,
-            },
-            {
-                path: '/all',
-                element: <h1>Все товары</h1>,
-            },
-            {
-                path: '/categories',
-                element: <h1>Категории</h1>,
-            },
-            {
-                path: '/categories/sport',
-                element: <h1>Категория спорта</h1>,
-            },
-            {
-                path: '/categories/sport/:slug',
-                element: <h1>Страница определенного спорта</h1>,
-            },
-            {
-                path: '/categories/clubs',
-                element: <h1>Спортивные клубы</h1>,
-            },
-            {
-                path: '/categories/clubs:slug',
-                element: <h1>Страница спортивного клуба</h1>,
-            },
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Root />,
+            errorElement: <Error />,
+            children: [
+                {
+                    path: '',
+                    element: <Main />,
+                },
+                {
+                    path: '/all',
+                    element: <h1>Все товары</h1>,
+                },
+                {
+                    path: '/categories',
+                    element: <h1>Категории</h1>,
+                },
+                {
+                    path: '/categories/sport',
+                    element: <h1>Категория спорта</h1>,
+                },
+                {
+                    path: '/categories/sport/:slug',
+                    element: <h1>Страница определенного спорта</h1>,
+                },
+                {
+                    path: '/categories/clubs',
+                    element: <h1>Спортивные клубы</h1>,
+                },
+                {
+                    path: '/categories/clubs:slug',
+                    element: <h1>Страница спортивного клуба</h1>,
+                },
 
-            /** Ну и так далее */
-        ],
-    },
-]);
+                /** Ну и так далее */
+            ],
+        },
+    ],
+    { basename: '/client' }
+);

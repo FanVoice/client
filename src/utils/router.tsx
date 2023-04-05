9b5d0c87ddca57676e81cd6f07fe7d92983b11f2
GuVictory <gubanova.vi10@gmail.com>
2023-04-05 14:25:07 +0300

    fix for deployment

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -5,50 +5,53 @@ import { Main } from '../pages/Main';
 import { RootForm } from '../pages/Forms/RootForm';
 import { VideoChatForm } from '../pages/Forms/VideoChatForm';
 
-export const router = createHashRouter([
-    {
-        path: '/',
-        element: <Root />,
-        errorElement: <Error />,
-        children: [
-            {
-                path: '',
-                element: <Main />,
-            },
-            {
-                path: '/all',
-                element: <h1>Все товары</h1>,
-            },
-            {
-                path: '/categories',
-                element: <h1>Категории</h1>,
-            },
-            {
-                path: '/categories/sport',
-                element: <h1>Категория спорта</h1>,
-            },
-            {
-                path: '/categories/sport/:slug',
-                element: <h1>Страница определенного спорта</h1>,
-            },
-            {
-                path: '/categories/clubs',
-                element: <h1>Спортивные клубы</h1>,
-            },
-            {
-                path: '/categories/clubs:slug',
-                element: <h1>Страница спортивного клуба</h1>,
-            },
-            {
-                path: '/forms',
-                element: <RootForm />,
-                children: [
-                    {
-                        path: '/video-chat',
-                        element: <VideoChatForm />,
-                    },
-                ],
-            },
-        ],
-    },
-]);
+export const router = createHashRouter(
+    [
+        {
+            path: '/',
+            element: <Root />,
+            errorElement: <Error />,
+            children: [
+                {
+                    path: '',
+                    element: <Main />,
+                },
+                {
+                    path: '/all',
+                    element: <h1>Все товары</h1>,
+                },
+                {
+                    path: '/categories',
+                    element: <h1>Категории</h1>,
+                },
+                {
+                    path: '/categories/sport',
+                    element: <h1>Категория спорта</h1>,
+                },
+                {
+                    path: '/categories/sport/:slug',
+                    element: <h1>Страница определенного спорта</h1>,
+                },
+                {
+                    path: '/categories/clubs',
+                    element: <h1>Спортивные клубы</h1>,
+                },
+                {
+                    path: '/categories/clubs:slug',
+                    element: <h1>Страница спортивного клуба</h1>,
+                },
+                {
+                    path: '/forms',
+                    element: <RootForm />,
+                    children: [
+                        {
+                            path: '/video-chat',
+                            element: <VideoChatForm />,
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    { basename: 'client' }
+);
